7a655af2baf8527e99ebd578137a3c4ac6c8801b
@Ash9311 <ashutoshmulki7@gmail.com>
2022-12-14 23:14:44 +0530

    dotenv

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
-const {PORT} = require('./config/serverCon')
+const {PORT} = require('./config/serverConfig')
 const express = require("express");
+require('dotenv').config()
 const setupAndStartServer = async () => {
     //create the express object
     const app = express();
     app.listen(PORT,()=>{
         console.log(`server started at ${PORT}`);
+        // console.log(process);
     })
 }
+
+setupAndStartServer();
